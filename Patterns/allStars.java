26ba84db0d7d0a841d21d0d27b2c20f5b250cec7
Saqlaen <msaqlaenahmed98@gmail.com>
2022-09-24 15:01:46 +0530

    added few patterns code

---
+++ Patterns/allStars.java
@@ -0,0 +1,23 @@
+public class allStars {
+    public static void main(String[] arg){
+        /*
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+         */
+        int n = 10;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
